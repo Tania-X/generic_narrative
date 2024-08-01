b14e2dd7c5f531102b9ae81dd4e24339ffff33fe
max <13126565350@163.com>
2024-08-01 17:10:44 +0800

    update comments for volatile variable RUNNING_STATUS

--- src/main/java/com/max/generic_narrative/CustomApplicationRunner.java
+++ src/main/java/com/max/generic_narrative/CustomApplicationRunner.java
@@ -16,6 +16,18 @@ public class CustomApplicationRunner implements ApplicationRunner {
 
   private static final String STABLE_RUNNING = "0";
 
+  /**
+   * 由于本类由@Component注解，被Spring容器管理，因此RUNNING_STATUS全局总是独一份的，跟是否为static无关
+   * 可以将static去掉，外部访问它时使用实例点访问。
+   * volatile变量RUNNIN_STATUS是直接放入JMM所定义的主内存的，所有工作线程直接访问主存来读取或修改该值，
+   * 该值被修改后也会通过缓存一致性协议“同步”给访问该值的工作线程
+   * 如果该值仅被CustomApplicationRunner#preCheck方法修改且被Emergency#checkHealth方法查看，
+   * 那么并不会触发并发问题
+   * 但由于Emergency#preCheck方法和CustomApplicationRunner#preCheck方法都会修改RUNNING_STATUS，
+   * 因此仅依赖于volatile是无法保证线程安全的
+   * 可以使用synchronized方法来修改RUNNING_STATUS，或者将RUNNING_STATUS改为AtomicInteger类型并
+   * 通过原子类赋值操作来操作它。
+   */
   public static volatile int RUNNING_STATUS = -1;
 
   @Override
